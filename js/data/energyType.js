2b4366487049b6aa729a97179474c8094aa832c8
Gi0vanniM <g.l.mierop@gmail.com>
2021-03-21 15:39:40 +0100

    changed energyType array to use the EnergyType class and changed array name to energyTypes

---
+++ js/data/energyType.js
@@ -0,0 +1,6 @@
+const energyTypes = Object.freeze({
+    LIGHTNING: new EnergyType('lightning'),
+    FIRE: new EnergyType('fire'),
+    FIGHTING: new EnergyType('fightning'),
+    WATER: new EnergyType('water'),
+});
