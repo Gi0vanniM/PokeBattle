7f2181a239ab85b8e62bc375170fd0f461497d76
Gi0vanniM <g.l.mierop@gmail.com>
2021-03-21 16:17:20 +0100

    attacks array defines some attacks

---
+++ js/data/attacks.js
@@ -0,0 +1,6 @@
+const attacks = Object.freeze({
+    ELECTRIC_RING: new Attack('Electric Ring', 50),
+    PIKA_PUNCH: new Attack('Pika Punch', 20),
+    HEAD_BUTT: new Attack('Head Butt', 10),
+    FLARE: new Attack('Flare', 30),
+});
