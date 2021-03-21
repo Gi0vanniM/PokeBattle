75fc4b31e45972c0bb3fbcd9fd94cf3c699ba4e2
Gi0vanniM <g.l.mierop@gmail.com>
2021-03-21 16:20:21 +0100

    added attacks to Pikachu and Charmeleon

--- js/classes/Pokemon/Charmeleon.js
+++ js/classes/Pokemon/Charmeleon.js
@@ -1,5 +1,13 @@
 class Charmeleon extends Pokemon {
     constructor(name) {
-        super(name, 'Charmeleon', energyTypes.FIRE, 60, [], [energyTypes.WATER, 2], [energyTypes.LIGHTNING, 10]);
+        super(
+            name,
+            'Charmeleon',
+            energyTypes.FIRE,
+            60,
+            [attacks.HEAD_BUTT, attacks.FLARE],
+            [energyTypes.WATER, 2],
+            [energyTypes.LIGHTNING, 10]
+        );
     }
 }
--- js/classes/Pokemon/Pikachu.js
+++ js/classes/Pokemon/Pikachu.js
@@ -1,5 +1,13 @@
 class Pikachu extends Pokemon {
     constructor(name) {
-        super(name, 'Pikachu', energyTypes.LIGHTNING, 60, [], [energyTypes.FIRE, 1.5], [energyTypes.FIGHTING, 20]);
+        super(
+            name,
+            'Pikachu',
+            energyTypes.LIGHTNING,
+            60,
+            [attacks.ELECTRIC_RING, attacks.PIKA_PUNCH],
+            [energyTypes.FIRE, 1.5],
+            [energyTypes.FIGHTING, 20]
+        );
     }
 }
