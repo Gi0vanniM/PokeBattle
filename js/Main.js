b31c1f68d484f0ef9b7ba8a294129e3748edaa2c
Gi0vanniM <g.l.mierop@gmail.com>
2021-03-22 11:38:10 +0100

    some testing

--- js/Main.js
+++ js/Main.js
@@ -1,1 +1,25 @@
 
+let pikaJeff = new Pikachu('jeff');
+let charHank = new Charmeleon('Hank');
+
+
+pikaJeff.attack(pikaJeff.attacks[1], charHank);
+
+
+// how to make own pokemon
+// let harryzard = new Pokemon(
+//     'Harryzard',
+//     'Charizard',
+//     energyTypes.FIRE,
+//     200,
+//     [
+//         attacks.FLARE,
+//         attacks.HEAD_BUTT
+//     ],
+//     [
+//         energyTypes.WATER,
+//         1.5],
+//     [
+//         energyTypes.FIRE,
+//         40]
+// );
