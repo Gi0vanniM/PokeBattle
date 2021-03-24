d803e468c3437b60dc240899c436dfd50f515cea
Gi0vanniM <g.l.mierop@gmail.com>
2021-03-24 13:18:55 +0100

    demonstration how to make a new pokemon

--- js/Main.js
+++ js/Main.js
@@ -1,25 +1,36 @@
 
-let pikaJeff = new Pikachu('jeff');
+let pikaJeff = new Pikachu('Jeff');
 let charHank = new Charmeleon('Hank');
 
 
 pikaJeff.attack(pikaJeff.attacks[1], charHank);
+charHank.attack(charHank.attacks[0], pikaJeff);
 
 
 // how to make own pokemon
-// let harryzard = new Pokemon(
-//     'Harryzard',
-//     'Charizard',
-//     energyTypes.FIRE,
-//     200,
-//     [
-//         attacks.FLARE,
-//         attacks.HEAD_BUTT
-//     ],
-//     [
-//         energyTypes.WATER,
-//         1.5],
-//     [
-//         energyTypes.FIRE,
-//         40]
-// );
+let harryzard = new Pokemon(
+    'Harryzard',
+    'Charizard',
+    // energyType
+    energyTypes.FIRE,
+    // max health
+    200,
+    // attacks
+    [
+        attacks.FLARE,
+        attacks.HEAD_BUTT
+    ],
+    // weakness
+    [
+        // weakness type
+        energyTypes.WATER,
+        // weakness multiplier
+        1.5
+    ],
+    // resitance
+    [
+        // resistance type
+        energyTypes.FIRE,
+        // resitance amount
+        40]
+);
