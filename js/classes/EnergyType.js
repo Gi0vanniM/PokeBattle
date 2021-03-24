fa32f4f46dfdcabd097ff022b2c977ad1247c93e
Gi0vanniM <g.l.mierop@gmail.com>
2021-03-24 14:12:05 +0100

    EnergyType getter for name

--- js/classes/EnergyType.js
+++ js/classes/EnergyType.js
@@ -2,4 +2,12 @@ class EnergyType {
     constructor(name) {
         this.name = name;
     }
+
+    /**
+     * get name method
+     * @returns string
+     */
+    getName() {
+        return this.name;
+    }
 }
