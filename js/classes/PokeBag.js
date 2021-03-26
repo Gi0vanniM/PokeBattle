ceb91fcdb66c495cbdd4d27b4f25f7bc74fe23fa
Gi0vanniM <g.l.mierop@gmail.com>
2021-03-26 09:33:16 +0100

    start of PokeBag

--- js/classes/PokeBag.js
+++ js/classes/PokeBag.js
@@ -1,4 +1,40 @@
 
 class PokeBag {
 
+    static maxPokemon = 10;
+
+    owner;
+    pokemonColleciton = [];
+
+    constructor(owner) {
+        this.owner = owner;
+    }
+
+    add(pokemon) {
+        if (this.isFull()) {
+            console.log('Bag is full');
+            return false;
+        }
+
+    }
+
+    remove(pokemon) {
+
+    }
+
+    removeAll() {
+
+    }
+
+    showAll() {
+
+    }
+
+    isFull() {
+        if (this.pokemonColleciton.length >= 10) {
+            return true;
+        }
+        return false;
+    }
+
 }
