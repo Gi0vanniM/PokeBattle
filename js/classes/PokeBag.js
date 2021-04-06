339ff17ffb6e4d78eb7905db385ac93d0c015de3
Gi0vanniM <g.l.mierop@gmail.com>
2021-04-06 15:03:02 +0200

    added comments to PokeBag class

--- js/classes/PokeBag.js
+++ js/classes/PokeBag.js
@@ -11,37 +11,42 @@ class PokeBag {
     }
 
     add(pokemon) {
+        // check if bag is full
         if (this.isFull()) {
             console.log('Bag is full');
             return false;
         }
+        // check if bag already contains pokemon
         if (this.pokemonCollection.includes(pokemon)) {
             console.log('PokeBag already contains that pokemon');
             return false;
         }
+        // check if pokemon is instance of Pokemon class
         if (!(pokemon instanceof Pokemon)) {
             return false;
         }
-
+        // add the pokemon to the bag's array
         this.pokemonCollection.push(pokemon);
         console.log('Added ' + pokemon.name + ' to bag');
     }
 
     remove(pokemon) {
+        // check if pokemon is instance of Pokemon class
         if (!Pokemon.isPokemon(pokemon)) {
             return false;
         }
+        // check if bag contains pokemon
         if (!this.pokemonCollection.includes(pokemon)) {
             console.log("PokeBag doesn't contain that pokemon");
             return false;
         }
-
+        // recreate the bag's array of pokemon but without the pokemon that gets removed
         this.pokemonCollection = this.pokemonCollection.filter(p => p !== pokemon);
         console.log('Removed ' + pokemon.name + ' from bag');
     }
 
     removeAll() {
-        
+
     }
 
     showAll() {
