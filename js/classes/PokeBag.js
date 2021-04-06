296a6dfd1db115b1d40ab4967305dbf72d4644b8
Gi0vanniM <g.l.mierop@gmail.com>
2021-04-06 14:57:52 +0200

    add and remove pokemon to/from PokeBag

--- js/classes/PokeBag.js
+++ js/classes/PokeBag.js
@@ -4,7 +4,7 @@ class PokeBag {
     static maxPokemon = 10;
 
     owner;
-    pokemonColleciton = [];
+    pokemonCollection = [];
 
     constructor(owner) {
         this.owner = owner;
@@ -15,15 +15,33 @@ class PokeBag {
             console.log('Bag is full');
             return false;
         }
+        if (this.pokemonCollection.includes(pokemon)) {
+            console.log('PokeBag already contains that pokemon');
+            return false;
+        }
+        if (!(pokemon instanceof Pokemon)) {
+            return false;
+        }
 
+        this.pokemonCollection.push(pokemon);
+        console.log('Added ' + pokemon.name + ' to bag');
     }
 
     remove(pokemon) {
+        if (!Pokemon.isPokemon(pokemon)) {
+            return false;
+        }
+        if (!this.pokemonCollection.includes(pokemon)) {
+            console.log("PokeBag doesn't contain that pokemon");
+            return false;
+        }
 
+        this.pokemonCollection = this.pokemonCollection.filter(p => p !== pokemon);
+        console.log('Removed ' + pokemon.name + ' from bag');
     }
 
     removeAll() {
-
+        
     }
 
     showAll() {
@@ -31,7 +49,7 @@ class PokeBag {
     }
 
     isFull() {
-        if (this.pokemonColleciton.length >= 10) {
+        if (this.pokemonCollection.length >= PokeBag.maxPokemon) {
             return true;
         }
         return false;
